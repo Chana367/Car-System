a374f629d0b2f5624c0501cf6ae08b70efe77cf5
Francisco Paredes <paredesfrancisco031@gmail.com>
2024-09-22 21:56:18 -0300

    fix url

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 import * as storageService from './local-storage.js';
 
-let ruta = '/public/autos.json';
+let ruta = `${window.location.origin}/public/autos.json`;
 let listadoAutos = [];
 
 async function getAutos() {
